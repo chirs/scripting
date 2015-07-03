993c3cc2ebe88a45974f87563cd8126489ca9656
Chris Edgemon <chrisedgemon@gmail.com>
2015-07-03 22:51:07 +0200

    add some 3d aspects to lightning.gh

--- src/py/lightning.py
+++ src/py/lightning.py
@@ -13,13 +13,14 @@ start = rg.Point3d(0,0,0)
 points = [start]
 curve_list = []
 thicknesses = []
+circles = []
 
 degrees = 180
 
 
 def point_from_vector(start, direction, scale):
 	x, y = [scale * op(direction) for op in (math.sin, math.cos)]
-	return rs.PointAdd(start, rg.Point3d(x, y, 0))
+	return rs.PointAdd(start, rg.Point3d(x, 0, y))
 	
 	
 def split_thickness(t):
@@ -35,6 +36,29 @@ def get_new_point(p):
 	np = point_from_vector(p, direction, stride)
 	return np
 	
+def get_circle(p, steps):
+	
+	step = xsteps - steps
+	
+	height = step * stride
+	
+	if step == 0:
+		scaled_radius = .01
+	else:
+		scaled_radius = step * radius
+	
+	center = rg.Point3d(0,0, height)
+	
+	
+	
+	rotation = random.uniform(0, 2 * math.pi)
+	
+	normal_vector= rs.PointSubtract(rg.Point3d(0,0,1), rg.Point3d(0,0,0))
+	normal_plane = rs.PlaneFromNormal(center, normal_vector)
+	circle = rs.AddCircle(normal_plane, scaled_radius)
+	circles.append(circle) 
+	return circle
+	
 	
 
 
@@ -44,15 +68,14 @@ def loop(steps, parent, thickness):
 		return
 		
 	new_point = get_new_point(parent)
+	get_circle(parent, steps)
 		
 	c = rs.AddCurve([parent, new_point])
 	curve_list.append(c)
 	thicknesses.append(thickness)
 	points.append(new_point)
 		
-	print(split)
 	if random.random() < split:
-		print('hi')
 		t1, t2 = split_thickness(thickness)
 		loop(steps - 1, new_point, t1)
 		loop(steps - 1, rg.Point3d(new_point), t2)
