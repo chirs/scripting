48a91f15228c3272c16656c8a3bf53204b0b5789
Chris Edgemon <chrisedgemon@gmail.com>
2015-07-16 09:33:41 +0200

    refactor gh_recursive

--- src/py/gh_recursive.py
+++ src/py/gh_recursive.py
@@ -1,20 +1,42 @@
-#Based on a script found via Google search for TryFitCircleTTT:
+# Based on a script found via Google search for TryFitCircleTTT:
+# http://www.grasshopper3d.com/forum/topics/cycle-operations-in-gh
 
 import Rhino
 import scriptcontext
 
-CIRCLES_CONTAINER = []
+CIRCLES = []
 
 tolerance = scriptcontext.doc.ModelAbsoluteTolerance
 
-line = Rhino.Geometry.Line( CURVE_A.PointAtStart, CURVE_B.PointAtStart )
 
-count = False
+def helper(curve, px, circle, last):
+	param = curve.ClosestPoint(px, 0.0)[1]
+	
+	domain = Rhino.Geometry.Interval(curve.Domain[0], param)
+	dist = curve.GetLength(domain) + last_circle.Radius
+	if dist >= curve.GetLength():
+		return None, None
+		
+	point = curve.PointAtLength(dist)
+	param = curve.ClosestPoint(point, 0.0)[1]
+	
+	if param < last:
+		return None, None
+		
+	return point, param
+	
+
+
+first = True
 
 while True:
+	
+  # step 1
 
-  if count == False:
-    crv_X = line.ToNurbsCurve()
+  if first:
+    first = False
+    
+    crv_X = Rhino.Geometry.Line( CURVE_A.PointAtStart, CURVE_B.PointAtStart).ToNurbsCurve()
     length = crv_X.GetLength() * 0.5
     
     pt_X = crv_X.PointAtLength(length)
@@ -27,37 +49,25 @@ while True:
     param_B = CURVE_B.ClosestPoint(pt_B, 0.0)[1]
 
   else:
+	  
     crv_X = last_circle.ToNurbsCurve()
     length = crv_X.GetLength() * 0.5
     
     pt_X = crv_X.PointAtLength(length)
     param_X = crv_X.ClosestPoint(pt_X, 0.0)[1]
     
-    param_A = CURVE_A.ClosestPoint(pt_X, 0.0)[1]
-    pt_A = CURVE_A.PointAt(param_A)
-    
-    domain = Rhino.Geometry.Interval(CURVE_A.Domain[0], param_A)
-    dist_A = CURVE_A.GetLength(domain) + last_circle.Radius
-    if dist_A >= CURVE_A.GetLength(): 
-      break
-    else:
-      pt_A = CURVE_A.PointAtLength(dist_A)
-      param_A = CURVE_A.ClosestPoint(pt_A, 0.0)[1]
-      if param_A <= last_A:
-        break
+		
+    pt_A, param_A = helper(CURVE_A, pt_X, last_circle, last_A)
+    if pt_A is None:
+		break
 
-    param_B = CURVE_B.ClosestPoint(pt_X, 0.0)[1]
-    pt_B = CURVE_B.PointAt(param_B)
-    
-    domain = Rhino.Geometry.Interval(CURVE_B.Domain[0], param_B)
-    dist_B = CURVE_B.GetLength(domain) + last_circle.Radius
-    if dist_B >= CURVE_B.GetLength(): 
-      break
-    else:
-      pt_B = CURVE_B.PointAtLength(dist_B)
-      param_B = CURVE_B.ClosestPoint(pt_B,0.0)[1]
-      if param_B  <= last_B:
-        break
+		
+    pt_B, param_B = helper(CURVE_B, pt_X, last_circle, last_B)
+    if pt_B is None:
+		break
+        
+        
+  # step 2
 
   circle = Rhino.Geometry.Circle.TryFitCircleTTT(crv_X, CURVE_A, CURVE_B, param_X, param_A, param_B)
   
@@ -65,11 +75,10 @@ while True:
     break
   else:
     if circle.Radius > tolerance: 
-      CIRCLES_CONTAINER.append(circle)
+      CIRCLES.append(circle)
       last_A = param_A
       last_B = param_B
     last_circle = circle
     
-  count = True
   
-CIRCLES = CIRCLES_CONTAINER
+ 
