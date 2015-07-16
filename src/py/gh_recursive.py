1baeae531aa7ea7eb84f5fad850dc5dd4f4f0652
Chris Edgemon <chrisedgemon@gmail.com>
2015-07-16 09:54:48 +0200

    more refactor

--- src/py/gh_recursive.py
+++ src/py/gh_recursive.py
@@ -4,81 +4,63 @@
 import Rhino
 import scriptcontext
 
-CIRCLES = []
 
-tolerance = scriptcontext.doc.ModelAbsoluteTolerance
-
-
-def helper(curve, px, circle, last):
-	param = curve.ClosestPoint(px, 0.0)[1]
+def closest_point(curve, length):
+	"""Get the closest point on a curve at a given length"""
 	
-	domain = Rhino.Geometry.Interval(curve.Domain[0], param)
-	dist = curve.GetLength(domain) + last_circle.Radius
+	return curve.ClosestPoint(curve.PointAtLength(length), 0.0)[1]
+
+
+def create_param(curve, point, radius, last):
+	"""Create a param given input"""
+	
+	domain = Rhino.Geometry.Interval(curve.Domain[0], curve.ClosestPoint(point, 0.0)[1])
+	dist = curve.GetLength(domain) + radius
+	
 	if dist >= curve.GetLength():
-		return None, None
+		return None
 		
-	point = curve.PointAtLength(dist)
-	param = curve.ClosestPoint(point, 0.0)[1]
+	param = closest_point(curve, dist)
 	
-	if param < last:
-		return None, None
-		
-	return point, param
+	if param >= last:
+		return param
+	else:
+		return None
 	
 
+CIRCLES = []
+circle = None
 
-first = True
 
 while True:
 	
-  # step 1
-
-  if first:
-    first = False
-    
-    crv_X = Rhino.Geometry.Line( CURVE_A.PointAtStart, CURVE_B.PointAtStart).ToNurbsCurve()
-    length = crv_X.GetLength() * 0.5
-    
-    pt_X = crv_X.PointAtLength(length)
-    param_X = crv_X.ClosestPoint(pt_X, 0.0)[1]
-    
-    pt_A = CURVE_A.PointAtLength(length)
-    param_A = CURVE_A.ClosestPoint(pt_A, 0.0)[1]
-    
-    pt_B = CURVE_B.PointAtLength(length)
-    param_B = CURVE_B.ClosestPoint(pt_B, 0.0)[1]
-
-  else:
-	  
-    crv_X = last_circle.ToNurbsCurve()
-    length = crv_X.GetLength() * 0.5
-    
-    pt_X = crv_X.PointAtLength(length)
-    param_X = crv_X.ClosestPoint(pt_X, 0.0)[1]
-    
-		
-    pt_A, param_A = helper(CURVE_A, pt_X, last_circle, last_A)
-    if pt_A is None:
-		break
-
-		
-    pt_B, param_B = helper(CURVE_B, pt_X, last_circle, last_B)
-    if pt_B is None:
-		break
+  if circle is None:
+    curve_X = Rhino.Geometry.Line( CURVE_A.PointAtStart, CURVE_B.PointAtStart).ToNurbsCurve()
+    param_A, param_B, param_X = [closest_point(crv, curve_X.GetLength() * 0.5) for crv in [CURVE_A, CURVE_B, curve_X]]	 
+  else: 
+    curve_X = circle.ToNurbsCurve()  
+    pt_X = curve_X.PointAtLength(curve_X.GetLength() * 0.5)
+    param_A = create_param(CURVE_A, pt_X, circle.Radius, param_A)
+    param_B = create_param(CURVE_B, pt_X, circle.Radius, param_B)	
+    param_X = curve_X.ClosestPoint(pt_X, 0.0)[1]
+	
+  if param_A is None or param_B is None:
+	break
         
-        
-  # step 2
-
-  circle = Rhino.Geometry.Circle.TryFitCircleTTT(crv_X, CURVE_A, CURVE_B, param_X, param_A, param_B)
+  circle = Rhino.Geometry.Circle.TryFitCircleTTT(curve_X, CURVE_A, CURVE_B, param_X, param_A, param_B)
   
   if circle == Rhino.Geometry.Circle.Unset:
     break
-  else:
-    if circle.Radius > tolerance: 
-      CIRCLES.append(circle)
-      last_A = param_A
-      last_B = param_B
-    last_circle = circle
+    
+  #if circle.Radius > scriptcontext.doc.ModelAbsoluteTolerance: 
+  CIRCLES.append(circle)
+	
+
+
+
+
+    
+
     
   
  
